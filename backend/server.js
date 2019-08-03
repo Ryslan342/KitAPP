20d1deba060878121368df0950c85808b409e588
Ryslan342 <ryslanartamonov786@gmail.com>
2019-08-03 09:19:04 +0300

    Add public root file

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ const { mongodb, server, client } = require("./config.json");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
-app.use(require("cookie-parser")(server["cookie-secret"]));
+app.use(require("cookie-parser")());
 
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", client.host);
@@ -19,6 +18,14 @@ app.use(function (req, res, next) {
 
 app.use("/sign/", require("./routers/sign.js"));
 app.use("/hotels/", require("./routers/hotels.js"));
+
+
+app.use(express.static(client.public_dir));
+app.get(client.root_file.paths, function (req, res) {
+    res.sendFile(client.root_file.file, {
+        root: client.public_dir,
+    });
+});
 
 mongoose.connect(mongodb.url, { useNewUrlParser: true }, function (err) {
     if (err) {
